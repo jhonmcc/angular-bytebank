125e677a63150ebda3a463b8ce0c4626dd4082d4
jhonmcc <jhonmcc@gmail.com>
2022-09-07 20:01:15 -0300

    uso de routes, uso de routerlink, uso de routerlink active

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TransferenciaComponent } from './transferencia/transferencia/transferencia.component';
+import { ExtratoComponent } from './transferencia/extrato/extrato.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'extrato', pathMatch: 'full' },
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'transferencia', component: TransferenciaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/transferencia/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia/transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Transferencia } from './../../models/transferencia.model';
 import { TransferenciaService } from './../../services/transferencia.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transferencia',
@@ -11,7 +12,7 @@ export class TransferenciaComponent {
   // declarando output pra cuspir informacao ao app-root
   @Output() aoTranferir = new EventEmitter<any>();
 
-  constructor(private service: TransferenciaService) { }
+  constructor(private service: TransferenciaService, private router: Router) { }
 
   valor: number = 0;
   destino: number = 0;
@@ -22,7 +23,9 @@ export class TransferenciaComponent {
     const objTransferir: Transferencia = { valor: this.valor, destino: this.destino };
     this.service.adicionar(objTransferir).subscribe(resultado => {
       console.log(resultado)
-      this.limparCampos();
+      // this.limparCampos();
+      // this.router.navigate(['extrato'], ['othersParameters']); passagen de parametros
+      this.router.navigateByUrl('extrato');
     },
       (err) => console.error(err)
     );
