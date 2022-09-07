cd6938ccd95d9baad959290e5db445356f0ccfd1
jhonmcc <jhonmcc@gmail.com>
2022-09-07 13:56:09 -0300

    criado componente extrato, uso de ngFor, uso de input para passar as informaçoes, adicionado pipe e referencia no app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bytebank';
+  transferencias: any[] = [];
+
+  transferir($event: any){
+    console.log($event);
+    const transferencia = {...$event, data: new Date()}
+    this.transferencias.push(transferencia);
+  }
 }
--- src/app/transferencia/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia/transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-transferencia',
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TransferenciaComponent {
   constructor() { }
 
+  // declarando output pra cuspir informacao ao app-root
+  @Output() aoTranferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
   transferir(){
     console.log("click transferir");
+    this.aoTranferir.emit({valor: this.valor, destino: this.destino});
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 
 }
